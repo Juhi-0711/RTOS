28ba5f8c01017db36dbd29fdc497162a96ae900f
Juhi Singh <Juhidpotterhead0711@gmail.com>
2019-02-17 23:59:00 +0530

    Update server.c

--- EchoEngine/server.c
+++ EchoEngine/server.c
@@ -18,12 +18,12 @@ int main(void)
   key_t key;
   int myQid;
   //char dataToBeRead[1024];
-  if((key= ftok("/Users/yashpal/Desktop/Sem8/RTOS/EchoServer/server.c", 65))==-1)
+  if((key= ftok("/Users/yashpal/Desktop/Sem8/RTOS/EchoServer/server.c", 65))==-1) //generating unique key
   {
     perror("ftok");
     exit(1);
   }
-  if((myQid = msgget(key, 0666 | IPC_CREAT))==-1)
+  if((myQid = msgget(key, 0666 | IPC_CREAT))==-1) //creating the common server queue
   {
     perror("msgget");
     exit(1);
@@ -33,17 +33,17 @@ int main(void)
   {
   msgrcv(myQid, &msg_rcv, sizeof(struct msg_buffer), 0, 0); 
 
-  printf("Got: %s\n", msg_rcv.msg_text);
+  printf("Got: %s\n", msg_rcv.msg_text); //got message
   
 
-  if(msg_rcv.msg_text[0]<='z' && msg_rcv.msg_text[0]>='a')
+  if(msg_rcv.msg_text[0]<='z' && msg_rcv.msg_text[0]>='a') //Change capitalisation
       msg_rcv.msg_text[0]= (char)(msg_rcv.msg_text[0]-32);
     msg_snd=msg_rcv;
 
-    printf("Sending %s\n", msg_snd.msg_text);
+    printf("Sending %s\n", msg_snd.msg_text); 
     int client_id=msg_rcv.qid;
 
-    if(msgsnd(client_id, &msg_snd, sizeof(struct msg_buffer),0)==-1)
+    if(msgsnd(client_id, &msg_snd, sizeof(struct msg_buffer),0)==-1) //reply to the client through the message queue with id= client_ID (as specified in the incoming request)
     {
       perror("msgsnd");
       exit(1);
