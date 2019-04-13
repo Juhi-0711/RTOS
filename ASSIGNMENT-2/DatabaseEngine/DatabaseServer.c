aa3f1d2fee7dec4ef1770042219c6f0064e14d4d
Juhi Singh <Juhidpotterhead0711@gmail.com>
2019-04-13 15:48:29 +0530

    Update DatabaseServer.c

--- ASSIGNMENT-2/DatabaseEngine/DatabaseServer.c
+++ ASSIGNMENT-2/DatabaseEngine/DatabaseServer.c
@@ -1,7 +1,7 @@
 #include <errno.h> 
-#include <arpa/inet.h>    //close 
+#include <arpa/inet.h>   
 #include <netinet/in.h> 
-#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros 
+#include <sys/time.h> 
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
@@ -26,15 +26,15 @@ int main(int argc , char *argv[])
     int sdMax;  
     struct sockaddr_in serverAddress;  
         
-    char readBuffer[1025];  //data buffer of 1K 
+    char readBuffer[1025];  
         
-    //set of socket descriptors 
+    //socket descriptors 
     fd_set fdsRead;  
         
-    //a message 
+
     char *message = "Hellooo.... \r\n";  
     
-    //initialise all client_socket[] to 0 so not checked 
+    //initialise
     for (i = 0; i < clientMax; i++)  
     {  
         socketClient[i] = 0;  
@@ -48,7 +48,7 @@ int main(int argc , char *argv[])
     }  
     
     //set master socket to allow multiple connections , 
-    //this is just a good habit, it will work without this 
+
     if( setsockopt(socketM, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 )  
     {  
         perror("setsockopt");  
@@ -67,8 +67,7 @@ int main(int argc , char *argv[])
         exit(EXIT_FAILURE);  
     }  
     printf("Listener on port %d \n", PORT);  
-        
-    //try to specify maximum of 3 pending connections for the master socket 
+        //max 3 pending connections
     if (listen(socketM, 3) < 0)  
     {  
         perror("listen");  
@@ -78,20 +77,13 @@ int main(int argc , char *argv[])
     //accept the incoming connection 
     addrlen = sizeof(serverAddress);  
     puts("Waiting for connections ...");  
-        
-    //clear the socket set 
-        
-    //add child sockets to set 
-    
-    //wait for an activity on one of the sockets , timeout is NULL , 
-    //so wait indefinitely 
+
    
     int fdJoint;
     fdJoint = open("joint.txt", O_RDWR);
     
     int fdProject;
     fdProject = open("project.txt", O_RDWR);
-    // printf("%d\n", fd);
     int ls;
 
 
@@ -103,20 +95,20 @@ int main(int argc , char *argv[])
     {  
         FD_ZERO(&fdsRead);  
 
-        //add master socket to set 
+   
         FD_SET(socketM, &fdsRead);  
         sdMax = socketM; 
 
         for ( i = 0 ; i < clientMax ; i++)  
         {  
-            //socket descriptor 
+         
             sd = socketClient[i];  
                 
             //if valid socket descriptor then add to read list 
             if(sd > 0)  
                 FD_SET( sd , &fdsRead);  
                 
-            //highest file descriptor number, need it for the select function 
+ 
             if(sd > sdMax)  
                 sdMax = sd;  
         }
@@ -128,8 +120,7 @@ int main(int argc , char *argv[])
             printf("select error");  
         } 
             
-        //If something happened on the master socket , 
-        //then its an incoming connection 
+
         if (FD_ISSET(socketM, &fdsRead))  
         {  
             if ((socketN = accept(socketM, (struct sockaddr *)&serverAddress, (socklen_t*)&addrlen))<0)  
@@ -138,51 +129,42 @@ int main(int argc , char *argv[])
                 exit(EXIT_FAILURE);  
             }  
             
-            //inform user of socket number - used in send and receive commands 
             printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , socketN , inet_ntoa(serverAddress.sin_addr) , ntohs(serverAddress.sin_port));  
-          
-            //send new connection greeting message 
-            // if( send(new_socket, message, strlen(message), 0) != strlen(message) )  
-            // {  
-            //     perror("send");  
-            // }  
                 
             puts("Welcome message sent successfully");  
                 
             //add new socket to array of sockets 
             for (i = 0; i < clientMax; i++)  
             {  
-                //if position is empty 
+              
                 if( socketClient[i] == 0 )  
                 {  
                     socketClient[i] = socketN;  
                     printf("Adding to list of sockets as %d\n" , i);  
                         
                     hello = "Send request";
-                    // send(client_socket[i], hello, strlen(hello), 0);
+                  
                     break;  
                 }  
             }  
         }  
             
-        //else its some IO operation on some other socket
+        // IO operation on some other socket
         for (i = 0; i < clientMax; i++)  
         {  
             sd = socketClient[i];  
                 
-            // printf("here\n");
+        
             if (FD_ISSET( sd , &fdsRead))  
             {  
-                //Check if it was for closing , and also read the 
-                //incoming message 
+          
                 memset(readBuffer, 0, 1024);
                 if ((readValue = read( sd , readBuffer, 1024)) == 0)  
                 {  
-                    //Somebody disconnected , get his details and print 
+                  
                     getpeername(sd , (struct sockaddr*)&serverAddress , (socklen_t*)&addrlen);  
                     printf("Host disconnected , ip %s , port %d \n" ,inet_ntoa(serverAddress.sin_addr) , ntohs(serverAddress.sin_port));  
-                        
-                    //Close the socket and mark as 0 in list for reuse 
+                 
                     close( sd );  
                     socketClient[i] = 0;  
                     
